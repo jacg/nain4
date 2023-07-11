e27d0f1b5858d60a39c4939c179be80faaf25dcf
Jacek Generowicz <jacg@my-post-office.net>
2023-07-11 19:22:01 +0200

    Add shell.nix using flake.nix via flake-compat

--- flake.nix
+++ flake.nix
@@ -3,11 +3,12 @@
   description = "Playing with G4 examples";
 
   inputs = {
-    nixpkgs    .url = "github:NixOS/nixpkgs/nixos-23.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs         .url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils     .url = "github:numtide/flake-utils";
+    flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, flake-compat }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in {
