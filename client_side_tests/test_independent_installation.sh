a5c66ee6644e22ed81f179773b07637d6c9125d4
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2023-07-11 22:23:48 +0300

    Fix directory creation

--- client_side_tests/test_independent_installation.sh
+++ client_side_tests/test_independent_installation.sh
@@ -5,7 +5,7 @@ tmp_dir=$(mktemp -d -t nain4-independent-XXXXXX)
 
 export NAIN4_INSTALL=$tmp_dir/nain4/install/
 
-mkdir    $tmp_dir/nain4/install
+mkdir -p $tmp_dir/nain4/install
 mkdir -p $tmp_dir/nain4/build && cd $_
 cmake -DCMAKE_INSTALL_PREFIX=${NAIN4_INSTALL} $test_dir/../nain4/
 make && make install
