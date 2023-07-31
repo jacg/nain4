c5c5493912eb2213877ade1379732cb7c511247c
Jacek Generowicz <jacg@my-post-office.net>
2023-07-31 17:43:30 +0200

    Make n4::ui pass on integer argument to /run/beamOn
    This allows a third CLI use case for `n4::ui`:
    
    1. `executable                # runs GUI`
    2. `executable macs/run.mac   # executes run.mac in batch mode`
    3. `executable 42             # executes /run/beamOn 42 in batch mode`

--- nain4/n4_ui.cc
+++ nain4/n4_ui.cc
@@ -6,6 +6,8 @@
 #include <G4UIExecutive.hh>
 
 #include <memory>
+#include <stdexcept>
+#include <string>
 
 namespace nain4 {
 
@@ -19,9 +21,14 @@ void ui(int argc, char** argv) {
     vis_manager.Initialize();
     ui_manager.ApplyCommand(execute + "macs/vis.mac");
     ui_executive.SessionStart();
-  } else { // Run in batch mode with the .mac file specified on CLI
-    G4String file_name = argv[1];
-    ui_manager.ApplyCommand(execute + file_name);
+  } else { // If given arg, run in batch mode
+    try { // If arg is an integer, use it as the argument to /run/beamOn
+      unsigned n = std::stoi(argv[1]);
+      ui_manager.ApplyCommand("/run/beamOn " + std::to_string(n));
+    } catch(std::invalid_argument) { // Otherwise, interpret the argument as a macro file
+      G4String file_name = argv[1];
+      ui_manager.ApplyCommand(execute + file_name);
+    }
   }
 }
 
