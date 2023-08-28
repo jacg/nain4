44874c39a056b26b0b8fa60a2a409d54684ec890
Jacek Generowicz <jacg@my-post-office.net>
2023-08-28 14:00:50 +0200

    Move systems from system.nix into flake.nix

--- flake.nix
+++ flake.nix
@@ -7,13 +7,13 @@
     nosys           .url = "github:divnix/nosys";
   };
 
-  inputs.systems.url = "path:./flake/systems.nix";
-  inputs.systems.flake = false;
-
   outputs = inputs @ {
     nosys,
     nixpkgs, # <---- This `nixpkgs` still has the `system` e.g. legacyPackages.${system}.zlib
     ...
-  }: let outputs = import ./flake/outputs.nix;
-     in nosys inputs outputs;
+  }:
+    let
+      outputs = import ./flake/outputs.nix;
+      system = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+    in nosys (inputs // { inherit system; }) outputs;
 }
