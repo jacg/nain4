4d9c5d195d002056e464e2947f4d8fe9a1477e07
Jacek Generowicz <jacg@my-post-office.net>
2023-08-29 18:21:56 +0200

    Add TODOs to template flake outputs

--- templates/basic/flake/outputs.nix
+++ templates/basic/flake/outputs.nix
@@ -53,8 +53,24 @@
 
   in rec {
 
-    # TODO a package to run my nain4 project
+    # packages.default = pkgs.stdenv.mkDerivation { stdenv = pkgs.clang_16.stdenv; } {
+    #   # CHANGEME-pname: replace "my-package" with a name better-suited to your project
+    #   pname = "my-package";
+    #   version = "0.0.1";
+    #   src = "${self}/src";
+    #   # TODO nativeBuildInputs =
+    # };
 
+    # Executed by `nix run <URL of this flake> -- <args?>`
+    # TODO apps.default = { type = "app"; program = "..."; };
+
+    # Executed by `nix run <URL of this flake>#my-app`
+    # TODO apps.my-app = { type = "app"; program = "<store-path>"; };
+
+    # Used by `direnv` when entering this directory (also by `nix develop <URL to this flake>`)
+    devShell = devShells.clang;
+
+    # Activated by `nix develop <URL to this flake>#clang`
     devShells.clang = pkgs.mkShell.override { stdenv = pkgs.clang_16.stdenv; } {
       name = "my-nain4-app-clang-devenv";
 
@@ -66,6 +82,7 @@
 
     };
 
+    # Activated by `nix develop <URL to this flake>#gcc`
     # devShells.gcc = pkgs.mkShell {
     #   name = "my-nain4-app-gcc-devenv";
 
@@ -76,7 +93,5 @@
     #   QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.libsForQt5.qt5.qtbase.bin}/lib/qt-${pkgs.libsForQt5.qt5.qtbase.version}/plugins";
 
     # };
-
-    devShell = devShells.clang;
 
   }
