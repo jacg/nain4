7f34583ea1e546a33ce8880cea569a75d2380678
Jacek Generowicz <jacg@my-post-office.net>
2023-08-29 18:53:56 +0200

    Talk about nixGL in the template welcome message

--- templates/default.nix
+++ templates/default.nix
@@ -12,15 +12,27 @@ You have created a simple nain4 application which you can use as the basis of yo
 
 Some downloading and compilation may take place. When this is complete you can try
 
-+ `just` - runs the application in batch mode with the provided `macs/run.mac` Geant4 macro file
-+ `just run` - runs the application in interactive mode
++ `just` - runs the application in batch mode with the provided `macs/run.mac` Geant4 macro file.
+
++ `just run` - runs the application in interactive mode.
+
+   If you are on NixOS or MacOS, graphics should work out of the box. On non-NixOS linuxes and WSL, you will probably have to use the `nixGL` helper, like this:
+
+   ```sh
+   nix run --impure github:guibou/nixGL -- just run
+   ```
+
+   The first time you use `nixGL` it will probably take quite some time to compile.
+
 + `just run 100` - runs the application in batch mode with `/run/beamOn 100`
+
+
 
 To evolve this into your own application
 
-+ `git init`
-+ `grep CHANGEME` will give you hints about places where you will probably want to change names, descriptions and metadata to something more appropriate for your project.
-+ Edit the code to suit your needs.
+1. `git init`
+2. `grep CHANGEME` will give you hints about places where you will probably want to change names, descriptions and metadata to something more appropriate for your project.
+3. Edit the code to suit your needs.
 '';
   };
 
