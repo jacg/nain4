ce2ae7dd1de00d5964f7b3d427619ec469420d61
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2023-09-09 12:02:41 +0300

    Remove CMake files

--- clangd-headers.cmake
+++
@@ -1,8 +0,0 @@
-
-# ----- Generate compile_commands.json, unless otherwise instructed on CLI --
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON CACHE INTERNAL "")
-
-# ----- Ensure that standard C++ headers are found by clangd
-if(CMAKE_EXPORT_COMPILE_COMMANDS)
-  set(CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES ${CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES})
-endif()
