f56e0dd55e84125eff6f0585a334ae9b2cfe361f
Jacek Generowicz <jacg@my-post-office.net>
2023-09-20 08:35:02 +0200

    MISSING Using overlay for overrideAttrs also gets broken by override

--- flake/outputs.nix
+++ flake/outputs.nix
@@ -3,18 +3,31 @@
 , ...
 }: let
 
-  pkgs = (nixpkgs.legacyPackages.extend (import ./overlays/argparse.nix));
+  pkgs = (nixpkgs.legacyPackages.extend add-debug-symbols-to-geant4);
   # Would prefer something along the lines of
   #    pkgs = import nixpkgs { overlays = [ (import ./overlays/argparse.nix) ]; };
   # but not sure how to get it to work with `nosys`
 
-  my-geant4 = pkgs.geant4.overrideAttrs (oldAttrs: {
-    dontStrip = true;
-    cmakeBuildType = "Debug";
-  });
+  add-debug-symbols-to-geant4 = final: previous: {
+    geant4 = previous.geant4.overrideAttrs (old: {
+      dontStrip = true;
+      cmakeBuildType = "RelWithDebInfo";
+    });
+  };
+
+  g4 = { thread ? false , inventor ? false , qt ? false, xm ? false, ogl ? false, python ? false, raytrace ? false }:
+    pkgs.geant4.override {
+      enableMultiThreading = thread;
+      enableInventor       = inventor;
+      enableQt             = qt;
+      enableXM             = xm;
+      enableOpenGLX11      = ogl;
+      enablePython         = python;
+      enableRaytracerX11   = raytrace;
+    };
+
 in {
-    packages.my-geant4 = my-geant4;
-
     packages.default = self.packages.my-geant4;
-
-  }
+    packages.geant4-with-debug-symbols = pkgs.geant4;
+    packages.my-geant4 = g4 { qt = true; ogl = true; };
+}
