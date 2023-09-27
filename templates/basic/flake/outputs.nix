b137d3871da5044fe183ba4da45631319d3015d4
Jacek Generowicz <jacg@my-post-office.net>
2023-09-27 16:37:26 +0200

    Work around failure by propagating inputs by hand

--- templates/basic/flake/outputs.nix
+++ templates/basic/flake/outputs.nix
@@ -25,7 +25,10 @@
       pname = "CHANGEME-my-package";
       version = "0.0.0";
       src = "${self}/src";
-      nativeBuildInputs = [ nain4.packages.nain4 ]; # Relying on propagated inputs for transitive dependencies
+      nativeBuildInputs = [ nain4.packages.nain4 ]
+        # We expect these to be propagated, because they are in
+        # propagatedNativeBuildInputs, but apparently they are not:
+                          ++ nain4.deps.build-prop;
     };
 
     # Executed by `nix run <URL of this flake> -- <args?>`
