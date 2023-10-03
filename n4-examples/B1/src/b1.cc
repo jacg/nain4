1ce7ba4b88837c06c78017a6ef4aa050b0b6adb2
Jacek Generowicz <jacg@my-post-office.net>
2023-10-03 17:30:08 +0200

    Tweaks to B1 example

--- n4-examples/B1/src/b1.cc
+++ n4-examples/B1/src/b1.cc
@@ -29,14 +29,12 @@ auto geometry() {
   G4double env_size_xy = 20*cm;
   G4double env_size_z  = 30*cm;
 
-  // World
   auto world = n4::box("World")
     .xy(1.2 * env_size_xy)
     .z (1.2 * env_size_z)
     .place(n4::material("G4_AIR"))
     .now();
 
-  // Envelope
   auto envelope = n4::box("Envelope")
     .xy(env_size_xy)
     .z (env_size_z )
@@ -44,19 +42,15 @@ auto geometry() {
     .in(world)
     .now();
 
-  // Shape 1
   n4::cons("Shape1")
-    .r1_inner(0*cm)
-    .r1      (2*cm)
-    .r2_inner(0*cm)
-    .r2      (4*cm)
-    .z       (6*cm)
+    .r1(2*cm)
+    .r2(4*cm)
+    .z (6*cm)
     .place(n4::material("G4_A-150_TISSUE"))
     .in(envelope)
     .at(0, 2*cm, -7*cm)
     .now();
 
-  // Shape 2
   n4::trd("Shape2")
     .x1(12*cm)
     .y1(10*cm)
@@ -70,49 +64,48 @@ auto geometry() {
 
   //always return the physical World
   return world;
-
 }
 
 
 auto generator() {
-  auto particle     = G4ParticleTable::GetParticleTable() -> FindParticle("gamma");
-  auto particle_gun = std::make_shared<G4ParticleGun>(particle, 1); // 1 particle
+  static auto gamma = n4::find_particle("gamma");
+  auto particle_gun = std::make_shared<G4ParticleGun>(gamma, 1); // 1 particle
+  particle_gun -> SetParticleMomentumDirection({0.,0.,1.});
+  particle_gun -> SetParticleEnergy(6*MeV);
 
   return [particle_gun] (auto event) {
-    particle_gun -> SetParticleMomentumDirection({0.,0.,1.});
-    particle_gun -> SetParticleEnergy(6*MeV);
-
-    auto envelope = dynamic_cast<G4Box*>(n4::find_logical("Envelope") -> GetSolid());
+    auto envelope = dynamic_cast<G4Box*>(n4::find_solid("Envelope"));
     auto env_size_xy = envelope -> GetXHalfLength() * 2;
     auto env_size_z  = envelope -> GetZHalfLength() * 2;
 
-    auto size = 0.8;
-    auto x0   = size * env_size_xy * (G4UniformRand() - 0.5);
-    auto y0   = size * env_size_xy * (G4UniformRand() - 0.5);
-    auto z0   = -0.5 * env_size_z;
+    auto size =  0.8;
+    auto x0   =  size * env_size_xy * (G4UniformRand() - 0.5);
+    auto y0   =  size * env_size_xy * (G4UniformRand() - 0.5);
+    auto z0   = -0.5  * env_size_z;
 
     particle_gun -> SetParticlePosition({x0, y0, z0});
     particle_gun -> GeneratePrimaryVertex(event);
   };
 }
 
+
 auto run_action(G4double& e_sum, G4double& e_sum2) {
   return (new n4::run_action())
     -> end([&] (auto run){
-         G4int n_events = run -> GetNumberOfEvent();
+         auto n_events = run -> GetNumberOfEvent();
          if (n_events == 0) return;
 
          // Compute dose = total energy deposit in a run and its variance
-         G4double rms = e_sum2 - e_sum*e_sum/n_events;
-         if (rms < 0) {
+         auto var = e_sum2 - e_sum*e_sum/n_events;
+         if (var < 0) {
            G4Exception("run_action", "end()", FatalException, "Negative RMS");
          }
-         rms = std::sqrt(rms);
+         auto rms = std::sqrt(var);
 
-         auto scoring_volume = n4::find_logical("Shape2");
-         G4double mass     = scoring_volume -> GetMass();
-         G4double dose     = e_sum / mass;
-         G4double dose_rms =  rms / mass;
+         auto scoring_vol = n4::find_logical("Shape2");
+         auto mass        = scoring_vol -> GetMass();
+         auto dose        = e_sum / mass;
+         auto dose_rms    =   rms / mass;
 
          std::cout << "\n"
                    << "The run consists of " << n_events << " gamma of 6 MeV\n"
@@ -120,9 +113,9 @@ auto run_action(G4double& e_sum, G4double& e_sum2) {
                    << G4BestUnit(dose, "Dose") << " rms = " << G4BestUnit(dose_rms, "Dose")
                    << "\n------------------------------------------------------------\n"
                    << G4endl;
-
        });
 }
+
 
 auto event_action(G4double& e_sum, G4double& e_sum2, G4double& e_evt) {
   return (new n4::event_action())
@@ -130,12 +123,13 @@ auto event_action(G4double& e_sum, G4double& e_sum2, G4double& e_evt) {
     ->   end([&] (auto) {e_sum += e_evt; e_sum2 += e_evt * e_evt;});
 }
 
+
 auto stepping_action(G4double& e_evt) {
   return new n4::stepping_action([&] (auto step) {
-    auto scoring_volume = n4::find_logical("Shape2");
+    auto scoring_vol = n4::find_logical("Shape2");
     auto current_volume = step -> GetPreStepPoint() -> GetTouchableHandle() -> GetVolume() -> GetLogicalVolume();
 
-    if (current_volume == scoring_volume) {
+    if (current_volume == scoring_vol) {
       e_evt += step -> GetTotalEnergyDeposit();
     }
   });
