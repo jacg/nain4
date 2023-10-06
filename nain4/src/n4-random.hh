531b9ae5db5d8089ef78c0ac5d0efec06f1338cb
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2023-10-06 21:13:16 +0300

    Implement n4::random::direction with restricted angles

--- nain4/src/n4-random.hh
+++ nain4/src/n4-random.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <CLHEP/Units/SystemOfUnits.h>
 #include <G4RandomDirection.hh>
 #include <G4ThreeVector.hh>
 #include <G4Types.hh>
@@ -18,7 +19,27 @@ inline G4double uniform           (G4double lo, G4double  hi) { return (hi - lo)
 inline G4double uniform_half_width(G4double dx              ) { return uniform(-dx, dx); }
 inline G4double uniform_width     (G4double dx              ) { return uniform_half_width(dx/2); }
 
-inline G4ThreeVector direction() { return G4RandomDirection(); }
+struct direction {
+  G4ThreeVector get();
+
+#define SET(NAME) direction& NAME(G4double x) {NAME##_ = x ; return *this;}
+  SET(min_cos_theta)
+  SET(max_cos_theta)
+  SET(min_phi)
+  SET(max_phi)
+#undef SET
+
+  // Might seem wrong, but it's not!
+  direction& min_theta(G4double x) {max_cos_theta_ = std::cos(x); return *this;}
+  direction& max_theta(G4double x) {min_cos_theta_ = std::cos(x); return *this;}
+
+private:
+  G4double min_cos_theta_ = -1;
+  G4double max_cos_theta_ =  1;
+  G4double min_phi_       =  0;
+  G4double max_phi_       = CLHEP::twopi;
+};
+
 
 inline bool     biased_coin(G4double chance_of_true)  { return uniform() < chance_of_true; }
 inline unsigned fair_die   (unsigned sides)           { return std::floor(uniform() * sides); }
