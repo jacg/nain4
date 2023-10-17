e2e1728628cc43433f571b0a4170da56e31e0fa9
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2023-10-17 18:45:38 +0300

    Fix exception name in message

--- nain4/src/n4-exceptions.hh
+++ nain4/src/n4-exceptions.hh
@@ -9,22 +9,25 @@ namespace exceptions {
 #define WRAP(NAME, FROM, MESSAGE)                              \
   "\n\n"                                                       \
   "########################################################\n" \
-  "[n4::exceptions::" #NAME "]\n\n"                            \
+  "[n4::exceptions::" + NAME + "]\n\n"                         \
   "[raised by " + FROM + "]: " + MESSAGE + "\n"                \
   "########################################################\n" \
   "\n\n"
 
 struct n4_exception : std::exception {
-  n4_exception(const std::string& from, const std::string& message)
-  : message(WRAP(not_found, from, message)) {}
+  n4_exception(const std::string& name, const std::string& from, const std::string& message)
+  : message(WRAP(name, from, message)) {}
   const char* what() const noexcept { return message.c_str();}
 
 private:
   std::string message;
 };
 
-#define N4_EXCEPTION(NAME) \
-struct NAME : n4_exception {using n4_exception::n4_exception;};
+#define N4_EXCEPTION(NAME)                                  \
+struct NAME : n4_exception {                                \
+  NAME(const std::string& from, const std::string& message) \
+    : n4_exception(#NAME, from, message) {}                 \
+};
 
 N4_EXCEPTION(not_found)
 N4_EXCEPTION(bad_cast)
