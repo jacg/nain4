a862141c4fbeed502ae41fbbbfcf663d79ba3e2a
Jacek Generowicz <jacg@my-post-office.net>
2023-10-31 12:24:12 +0100

    Refactor and correct generator distribution

--- n4-examples/double-sipm/src/generator.cc
+++ n4-examples/double-sipm/src/generator.cc
@@ -14,17 +14,19 @@ void generate_back_to_back_511_keV_gammas(G4Event* event) {
     auto source_radius =  1.5*mm;
     auto  scint_size   =  3  *mm;
     auto   ring_radius = 12.5*mm;
-    auto position = G4RandomDirection() * source_radius * G4UniformRand();
-    position.setX(0);
+    auto [y, z] = n4::random::random_on_disc(source_radius);
 
-    auto direction = G4ThreeVector{ n4::random::uniform_width( scint_size + position.getX())
-                                  , n4::random::uniform_width( scint_size + position.getY())
-                                  ,                           ring_radius + position.getZ()
+    // Not strictly correct (uniformly distributed in the plane, but not by
+    // angle), OK with small angle approximation, which we exceed slightly:
+    // atan(1.5 / 12.5) = 7°
+    auto direction = G4ThreeVector{ n4::random::uniform_width( scint_size + 0)
+                                  , n4::random::uniform_width( scint_size + y)
+                                  ,                           ring_radius + z
                                   }.unit(); // random unit vector which hits scintillator
     auto p = 0.511*MeV * direction;
 
     auto time   = 0;
-    auto vertex = new G4PrimaryVertex(position, time);
+    auto vertex = new G4PrimaryVertex({0,y,z}, time);
     vertex -> SetPrimary(new G4PrimaryParticle(gamma,  p.x(),  p.y(),  p.z()));
     vertex -> SetPrimary(new G4PrimaryParticle(gamma, -p.x(), -p.y(), -p.z()));
     event  -> AddPrimaryVertex(vertex);
