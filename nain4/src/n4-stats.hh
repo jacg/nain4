91be23ac197a400f6a0d56aa526bf85532729ee6
Jacek Generowicz <jacg@my-post-office.net>
2023-11-21 11:14:32 +0100

    Explicitly cast initial value to output type

--- nain4/src/n4-stats.hh
+++ nain4/src/n4-stats.hh
@@ -9,7 +9,10 @@ namespace stats {
 
 template<class CONTAINER>
 typename CONTAINER::value_type
-sum(const CONTAINER& data) { return std::accumulate(begin(data), end(data), 0.0); }
+sum(const CONTAINER& data) {
+    return std::accumulate(begin(data), end(data),
+                           static_cast<typename CONTAINER::value_type>(0));
+}
 
 template<class CONTAINER>
 std::optional<typename CONTAINER::value_type> // TODO type trait gymnastics in case contained value is integral?
