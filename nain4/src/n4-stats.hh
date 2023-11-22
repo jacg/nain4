765529e09768cbbd09c988fc24c19c1aab8aa4b6
Jacek Generowicz <jacg@my-post-office.net>
2023-11-22 09:44:49 +0100

    Use boost in implementation of n4::stats

--- nain4/src/n4-stats.hh
+++ nain4/src/n4-stats.hh
@@ -6,6 +6,8 @@
 #include <numeric>
 #include <optional>
 
+#include <boost/math/statistics/univariate_statistics.hpp>
+
 namespace nain4 {
 namespace stats {
 
@@ -20,49 +22,35 @@ template<class CONTAINER>
 std::optional<typename CONTAINER::value_type> // TODO type trait gymnastics in case contained value is integral?
 mean(const CONTAINER& data) {
   if (data.empty()) { return {}; }
-  return sum(data) / data.size();
+  return boost::math::statistics::mean(data);
 }
 
 template<class CONTAINER>
 std::optional<typename CONTAINER::value_type> // TODO type trait gymnastics in case contained value is integral?
 variance_population(const CONTAINER& data) {
-  auto average = stats::mean(data);
-  if (! average.has_value()) { return {}; }
-  auto mean = average.value();
-  auto squared_deltas = n4::map<typename CONTAINER::value_type>(
-      [mean](const auto x) { auto dx = x-mean; return dx*dx; },
-      data
-  );
-  return sum(squared_deltas) / data.size();
+  if (data.empty()) { return {}; }
+  return boost::math::statistics::variance(data);
 }
 
 template<class CONTAINER>
 std::optional<typename CONTAINER::value_type> // TODO type trait gymnastics in case contained value is integral?
 variance_sample(const CONTAINER& data) {
   if (data.size() < 2) { return {}; }
-  auto average = stats::mean(data);
-  auto mean = average.value();
-  auto squared_deltas = n4::map<typename CONTAINER::value_type>(
-      [mean](const auto x) { auto dx = x-mean; return dx*dx; },
-      data
-  );
-  return sum(squared_deltas) / (data.size() - 1);
+  return boost::math::statistics::sample_variance(data);
 }
 
 template<class CONTAINER>
 std::optional<typename CONTAINER::value_type> // TODO type trait gymnastics in case contained value is integral?
 std_dev_population(const CONTAINER& data) {
   auto var = variance_population(data);
-  if (var.has_value()) { return std::sqrt(var.value()); }
-  else                 { return {}; }
+  if (var.has_value()) { return std::sqrt(var.value()); } else { return {}; }
 }
 
 template<class CONTAINER>
 std::optional<typename CONTAINER::value_type> // TODO type trait gymnastics in case contained value is integral?
 std_dev_sample(const CONTAINER& data) {
   auto var = variance_sample(data);
-  if (var.has_value()) { return std::sqrt(var.value()); }
-  else                 { return {}; }
+  if (var.has_value()) { return std::sqrt(var.value()); } else { return {}; }
 }
 
 }
