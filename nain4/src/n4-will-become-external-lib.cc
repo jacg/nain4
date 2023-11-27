4a8e943c14e243dcfd82c811aaa8e9977571bc11
Jacek Generowicz <jacg@my-post-office.net>
2023-11-27 12:01:33 +0100

    Add interaction process fraction calculation

--- nain4/src/n4-will-become-external-lib.cc
+++ nain4/src/n4-will-become-external-lib.cc
@@ -2,6 +2,7 @@
 #include <n4-will-become-external-lib.hh>
 #include <n4-inspect.hh>
 #include <n4-material.hh>
+#include <n4-random.hh>
 #include <n4-sequences.hh>
 #include <n4-shape.hh>
 #include <n4-stream.hh>
@@ -86,4 +87,59 @@ std::vector<double> measure_abslength(test_config const& config) {
     }
     return result;
   }
+}
+
+// ----- Calculate 511 keV gamma interaction process fractions for given material ------------------------------
+interaction_process_fractions calculate_interaction_process_fractions(G4Material* material, G4VUserPhysicsList* physics) {
+  unsigned phot{0}, compt{0}, rayl{0};
+
+  auto kill_secondaries = [] (const G4Track* track) {
+    return track -> GetParentID() > 0 ? G4ClassificationOfNewTrack::fKill : G4ClassificationOfNewTrack::fUrgent;
+  };
+
+  auto record_process_and_kill = [&phot, &compt, &rayl] (const G4Step* step) {
+    auto process = step -> GetPostStepPoint() -> GetProcessDefinedStep() -> GetProcessName();
+    if (process != "Transportation") {
+      if (process == "compt") { compt++; }
+      if (process == "phot" ) {  phot++; }
+      if (process == "Rayl" ) {  rayl++; }
+
+      step -> GetTrack() -> SetTrackStatus(fStopAndKill);
+    }
+  };
+
+  auto isotropic_511keV_gammas = [] () {
+    auto particle_type = n4::find_particle("gamma");
+    auto energy        = 511 * keV;
+    auto isotropic     = n4::random::direction{};
+
+    return [energy, particle_type, isotropic] (G4Event* event) {
+      auto r        = isotropic.get() * energy;
+      auto particle = new G4PrimaryParticle{particle_type, r.x(), r.y(), r.z()};
+      auto vertex   = new G4PrimaryVertex{{}, 0};
+      particle -> SetPolarization(isotropic.get());
+      vertex   -> SetPrimary(particle);
+      event    -> AddPrimaryVertex(vertex);
+    };
+  };
+
+  auto enormous_sphere = [material] () {return n4::sphere("huge").r(1*km).place(material).now();};
+
+  // Gather together all the above actions
+  auto test_action = [&] {
+    return  (new n4::actions{isotropic_511keV_gammas()})
+      -> set((new n4::stacking_action{}) -> classify(kill_secondaries))
+      -> set(new n4::stepping_action{record_process_and_kill})
+      ;
+  };
+
+  n4::run_manager::create()
+    .fake_ui()
+    .physics(physics)
+    .geometry(enormous_sphere)
+    .actions(test_action)
+    .run(100000);
+
+  auto total = static_cast<float>(phot + compt + rayl);
+  return { phot/total, compt/total, rayl/total };
 }
